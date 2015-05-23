3ac2b7fa4381ea9e028901bf6dc954d4f3385c85
Jean-baptiste PINIAU <jpiniau@e3r3p2.42.fr>
2015-05-23 20:25:48 +0200

    gnl

---
+++ get_next_line.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jpiniau <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/11/24 17:55:35 by jpiniau           #+#    #+#             */
+/*   Updated: 2015/01/21 11:41:03 by jpiniau          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# define BUFF_SIZE 8
+
+# include <string.h>
+# include <sys/types.h>
+# include <sys/uio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include "libft.h"
+
+int	get_next_line(int const fd, char **line);
+
+#endif
